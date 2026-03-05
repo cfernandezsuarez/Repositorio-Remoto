a17e09b721fe3d8c137aad2121f8b1df982cd5f2
dfernandezpacheco <dfernandezpacheco@safareyes.es>
2026-03-05 16:38:53 +0100

    Probamos el error 4

--- src/NuevoArchivo.java
+++ src/NuevoArchivo.java
@@ -1,6 +1,6 @@
 public class NuevoArchivo {
     public static void main(String[] args) {
-        System.out.println("Segundo error ordenador 2");
+        System.out.println("Segundo error computadora 2");
         System.out.println("Tercer error primer ordenador");
     }
 }
