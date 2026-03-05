7a3f4f80466eafdb0e6006f008dbec0df149cad2
Ismael-2007 <icuaresmaojeda@safareyes.es>
2026-03-05 17:13:30 +0100

    Carlos las parte

--- src/NuevoArchivo.java
+++ src/NuevoArchivo.java
@@ -1,6 +1,7 @@
 public class NuevoArchivo {
     public static void main(String[] args) {
-        System.out.println("Segundo error computadora 2");
-        System.out.println("Tercer error primer ordenador");
+        System.out.println("Segundo error");
+        System.out.println("Tercer error");
+        System.out.println("Carlos guapo");
     }
 }
